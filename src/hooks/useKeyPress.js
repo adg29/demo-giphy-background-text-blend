76c97eacd5557c7772b768b1f7d03d40843c1f16
Alan Garcia <alandavidgarcia@gmail.com>
2019-11-17 11:18:03 -0500

    Update useKeyPress to address exhaustive-deps warning wrt to event  handlers

--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -4,29 +4,29 @@ import { useState, useEffect } from 'react'
 function useKeyPress(targetKey) {
   // State for keeping track of whether key is pressed
   const [keyPressed, setKeyPressed] = useState(false);
-  let prevKey = '';
-
-  // If pressed key is our target key then set to true
-  function downHandler({ key }) {
-    // prevent re-render
-    if (prevKey === targetKey) return;
-
-    if (key === targetKey) {
-      setKeyPressed(true);
-      prevKey = key;
-    }
-  }
-
-  // If released key is our target key then set to false
-  const upHandler = ({ key }) => {
-    if (key === targetKey) {
-      setKeyPressed(false);
-      prevKey = '';
-    }
-  };
 
   // Add event listeners
   useEffect(() => {
+    let prevKey = '';
+    // If pressed key is our target key then set to true
+    function downHandler({ key }) {
+      // prevent re-render
+      if (prevKey === targetKey) return;
+
+      if (key === targetKey) {
+        setKeyPressed(true);
+        prevKey = key;
+      }
+    }
+
+    // If released key is our target key then set to false
+    const upHandler = ({ key }) => {
+      if (key === targetKey) {
+        setKeyPressed(false);
+        prevKey = '';
+      }
+    };
+
     window.addEventListener('keydown', downHandler);
     window.addEventListener('keyup', upHandler);
     // Remove event listeners on cleanup
